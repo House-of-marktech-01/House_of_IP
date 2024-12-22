79e8575970096610d9f6e96a6d0ffbdbbc8c5351
Om6214 <omnathganapure9981@gmail.com>
2024-12-22 18:42:18 +0530

    final

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 5000 || process.env.PORT;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://house-of-ip.vercel.app/",
   credentials: true,
 };
 app.use(cors(corsOptions))
